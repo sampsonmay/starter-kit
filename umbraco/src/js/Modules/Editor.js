9e2d440a16b7c3e8f7208d09944e20a4254a62ab
Robert Shaw <48474369+sm-rob@users.noreply.github.com>
2023-08-10 12:30:15 +0100

    Added Figure + figurecaption stuff

--- umbraco/src/js/Modules/Editor.js
+++ umbraco/src/js/Modules/Editor.js
@@ -1,16 +1,36 @@
-const Tables = () => {
+const Editor = () => {
+
+    /**
+     * Tables
+     * Wrap all tables inside of a table class
+     */
     const tables = document.querySelectorAll(".editor table");
-    if( tables.length < 1 ) return;
-
-    for( let i = 0; i < tables.length; i++ ) {
-            
-        let wrapper = document.createElement("div");
-            wrapper.classList.add("table"); 
-
-        tables[i].parentNode.insertBefore(wrapper,tables[i]);
-        wrapper.appendChild(tables[i]);
-
+    if(tables.length) {
+        for( let i = 0; i < tables.length; i++ ) {
+            let wrapper = document.createElement("div");
+                wrapper.classList.add("table"); 
+                tables[i].parentNode.insertBefore(wrapper,tables[i]);
+            wrapper.appendChild(tables[i]);
+        }
     }
+
+
+    /**
+     * Figures + Captions
+     * Take the width of the image and apply it to the
+     * <figure> element
+     */
+    let figures = editor.querySelectorAll("figure");
+    if(figures) {
+        figures.forEach(x => {
+            let img = x.querySelector("img");
+            let w = img.width;
+
+            x.style.width = w + 'px';
+            
+        });
+    }
+
 }
 
-export default Tables;
+export default Editor;
