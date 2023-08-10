1716fe8de616133843011caf92c6ed9f2d4fd832
Robert Shaw <48474369+sm-rob@users.noreply.github.com>
2023-08-10 17:06:54 +0100

    Fixed JS error in Editors

--- umbraco/src/js/Modules/Editor.js
+++ umbraco/src/js/Modules/Editor.js
@@ -1,35 +1,41 @@
 const Editor = () => {
+    
+    const editors = document.querySelectorAll(".editor");
 
-    /**
-     * Tables
-     * Wrap all tables inside of a table class
-     */
-    const tables = document.querySelectorAll(".editor table");
-    if(tables.length) {
-        for( let i = 0; i < tables.length; i++ ) {
-            let wrapper = document.createElement("div");
-                wrapper.classList.add("table"); 
-                tables[i].parentNode.insertBefore(wrapper,tables[i]);
-            wrapper.appendChild(tables[i]);
+    editors.forEach(editor => {
+
+        /**
+         * Tables
+         * Wrap all tables inside of a table class
+         */
+        const tables = editor.querySelectorAll(".editor table");
+        if(tables.length) {
+            for( let i = 0; i < tables.length; i++ ) {
+                let wrapper = document.createElement("div");
+                    wrapper.classList.add("table"); 
+                    tables[i].parentNode.insertBefore(wrapper,tables[i]);
+                wrapper.appendChild(tables[i]);
+            }
         }
-    }
 
 
-    /**
-     * Figures + Captions
-     * Take the width of the image and apply it to the
-     * <figure> element
-     */
-    let figures = editor.querySelectorAll("figure");
-    if(figures) {
-        figures.forEach(x => {
-            let img = x.querySelector("img");
-            let w = img.width;
+        /**
+         * Figures + Captions
+         * Take the width of the image and apply it to the
+         * <figure> element
+         */
+        let figures = editor.querySelectorAll("figure");
+        if(figures) {
+            figures.forEach(x => {
+                let img = x.querySelector("img");
+                let w = img.width;
 
-            x.style.width = w + 'px';
-            
-        });
-    }
+                x.style.width = w + 'px';
+                
+            });
+        }
+
+    });
 
 }
 
